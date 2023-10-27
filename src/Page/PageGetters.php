a15a9db763cde90e921cdb680e6d0800b343d6f0
Maciej Dębowski <contact.meqbbc@gmail.com>
2023-10-27 23:44:08 +0200

    Update PageGetters.php
    Add new Page static methods for getting - GetChildren(int $parent_id)

--- src/Page/PageGetters.php
+++ src/Page/PageGetters.php
@@ -64,4 +64,22 @@
                 ["url", "=", $url]
             ]));    
         }
+
+        /**
+         * @name GetChildren
+         * 
+         * Returns Page children
+         */
+        public static function GetChildren(int $parent_id): Page | null {
+            global $app;
+
+            $database = $app->GetDatabaseHandler();
+            $table = $database->GetTable('sapphire_pages');      
+            
+            $pages = [];
+            foreach($table->AllWhere([ ["parent_id", "=", $parent_id] ]) as $page)
+                $pages[] = Page::From($page);
+
+            return $pages;    
+        }
     }
